2e79d74fc204bbdbd1d1011c3139829f37647942
Толстых Павел Сергеевич <tolstyh.ps@dc.oaoktk.ru>
2026-07-14 10:10:51 +0700

    CreateVersion7 Guid

--- PrinterCare.Server/Entities/BaseEntity.cs
+++ PrinterCare.Server/Entities/BaseEntity.cs
@@ -2,7 +2,7 @@
 {
     public abstract class BaseEntity
     {
-        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid Id { get; set; } = Guid.CreateVersion7();
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     }
